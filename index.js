ff8b42ad1baf4e2d4e6223c848735ba0a8c49cf5
Yana Aleksieva <ya.aleksieva@gmail.com>
2017-02-13 19:44:42 +0200

    set-up environment tooling - Express, eslint, mocha, chai

---
+++ index.js
@@ -0,0 +1,7 @@
+// random message to test the entry file - suppress eslint only for the sake of testing - DON'T DO IT AGAIN !!!
+console.log('Welcome to the coolest music app which allows you to send your most favorite songs to random people in a chat room!'); //eslint-disable-line no-console
+
+//later, get rid of the test message, and uncomment the following:
+//const express = require('express');
+//const bodyparser = require('body-parser');
+//const app = express();
