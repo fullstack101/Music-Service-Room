5c8277957e3c35306a6f70a1aedc8e2b39b92aab
kirilzh <knz140@aubg.edu>
2017-02-20 23:19:59 +0200

    added the ability to have multiple users connected to the server having their own ids and being able to chat with each other

--- server/server.js
+++ server/server.js
@@ -2,55 +2,84 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const readline = require('readline');
 const bodyParser = require('body-parser');
 const path = require('path');
 const request = require('request');
 const querystring = require('querystring');
 const cookieParser = require('cookie-parser');
+const _ = require("underscore");
+var participants = [];
 
-app.get('/chat', function(req, res){
-  res.sendFile(path.join(__dirname + '/../client/chat.html'));
+//Server's IP address
+app.set("ipaddr", "127.0.0.1");
+
+//Server's port number
+app.set("port", 3000);
+
+//specify views for folder
+app.set('views', express.static(__dirname + '/../client'));
+
+//view engine in jade
+//app.set("view engine", "jade");
+
+//specify where the static content is
+app.use(express.static(__dirname + '/../client'));
+
+//Tells the server to support JSON requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", function (req, res) {
+  //res.render(__dirname + '/../client/index');
+  res.sendFile(path.resolve(__dirname + "/../client/chat.html"));
 });
 
-//==================JUST IGNORE================================================
-app.use(bodyParser.urlencoded({extended: true}));
-app.post('/chat', function(req, res){
+//POST method to create a chat message
+app.post("/message", function(req, res) {
+
+  //The request body expects a param named "message"
+  let message = req.body.message;
+
+  //If the message is empty or wasn't sent it's a bad request
+  if(_.isUndefined(message) || _.isEmpty(message.trim())) {
+    return res.json(400, {error: "Message is invalid"});
+  }
+
   let name = req.body.name;
-  res.send('You sent the name ' + name + '.');
-  console.log(name);
+  //console.log("socket: " + typeof(sockets));
+  io.sockets.emit("incomingMessage", {message: message, name: name});
+
+  //Looks good, let the client know
+  res.status(200).json({message: "Message received"});
+
 });
 
-const rl = readline.createInterface({
-  input:  process.stdin,
-  output: process.stdout,
-  terminal: false
-});
+io.on("connection", function(socket){
 
-rl.question('What is your username', (answer) =>{
-  //TODO: Log the answer in a database
-  console.log(`Thank you, ${answer}`);
-});
-
-//=============================================================================
-
-io.on('connection', function(socket){
-  console.log('user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  //when a new user connects
+  socket.on("newUser", function(data) {
+      participants.push({id: data.id, name: data.name});
+      io.sockets.emit("newConnection", {participants: participants});
   });
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
+
+  //when a user changes their name
+  socket.on("nameChange", function(data){
+    _.findWhere(participants, {id: socket.id}).name = data.name;
+    io.sockets.emit("nameChnaged", {id: data.id, name: data.name});
   });
+
+  //when a user disconnects
+  socket.on("disconnect", function(){
+    participants = _.without(participants,_.findWhere(participants, {id: socket.id}));
+    io.sockets.emit("userDisconnected", {id: socket.id, sender:"system"});
+  });
+
 });
 
-// io.on('connection', function(socket){
-//   console.log('user connected');
-//   socket.on('chat message', function(msg){
-//     io.emit('chat message', msg);
-//   });
-// });
+http.listen(3000, function(){
+  console.log("Server up and running. Go to http://" +
+    app.get("ipaddr") + ":" + app.get("port"));
+});
 
 
 //=======================SPOTIFY API===================================
@@ -174,4 +203,4 @@ app.get('/refresh_token', function(req, res) {
   });
 });
 
-http.listen(3000);
+//http.listen(3000);
